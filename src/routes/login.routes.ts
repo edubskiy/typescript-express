5a330313166164dc575de6364be264e928b13b41
Evgeniy <edubskiy@openwaygroup.com>
2020-06-11 21:17:41 +0300

    Added /logout route

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -37,4 +37,27 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 })
 
+router.get('/', (req: Request, res: Response) => {
+  if (req.session != null && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `)
+  } else {
+    res.send(`
+      <div>
+        <div>You are NOT logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `)
+  }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined
+  res.redirect('/')
+})
+
 export { router }
