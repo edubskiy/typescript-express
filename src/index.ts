e1efd9fe7a4f26c15e48fb74e31f45a2fec21652
Evgeniy <edubskiy@openwaygroup.com>
2020-06-11 20:10:46 +0300

    Setup default route in seperate file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import express, { Request, Response } from 'express'
+import { router } from './routes/login.routes';
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>Hi there</div>
-  `)
-})
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
