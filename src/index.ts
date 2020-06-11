454cd1c8359a1f2f846e7e827232fe611beecbef
Evgeniy <edubskiy@openwaygroup.com>
2020-06-11 20:04:22 +0300

    Setup default route, get basic response from this route, listen on port with message

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hey');
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>Hi there</div>
+  `)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
