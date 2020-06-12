8d540df3345983d820c5e5a9abfd423af56f7ec8
Evgeniy <edubskiy@openwaygroup.com>
2020-06-13 01:53:51 +0300

    Added get and controller decorators to build up routes on the fly

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express'
 import { router } from './routes/routes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session'
+import { router as controllerRouter} from './decorators/controller'
+import './controllers/login.controller'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['dskfjkdf'] }))
 app.use(router)
+app.use(controllerRouter)
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
