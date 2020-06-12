76fe216ee72885034a00a0749d0768035ded55ed
Evgeniy <edubskiy@openwaygroup.com>
2020-06-13 01:16:22 +0300

    Practical metadata experiment

--- experimental/metadata.ts
+++ experimental/metadata.ts
@@ -1,17 +1,35 @@
 import 'reflect-metadata'
 
-const plane = {
-  color: 'red'
+class Plane {
+  color: string = 'red'
+
+  @markFunction
+  fly(): void {
+    console.log('vrrrr');
+  }
 }
 
-Reflect.defineMetadata('note', 'hi there', plane, 'color')
+function markFunction(target: Plane, key: string) {
+  Reflect.defineMetadata('secret', 123, target, key)
+}
 
-const note = Reflect.getMetadata('note', plane, 'color')
+const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly')
 
-console.log("Note:");
-console.log(note);
+console.log('The secret is');
+console.log(secret);
 
 
+
+// const plane = {
+//   color: 'red'
+// }
+
+// Reflect.defineMetadata('note', 'hi there', plane, 'color')
+
+// const note = Reflect.getMetadata('note', plane, 'color')
+
+// console.log("Note:");
+// console.log(note);
 
 // Reflect.defineMetadata('note', 'hi there', plane)
 
