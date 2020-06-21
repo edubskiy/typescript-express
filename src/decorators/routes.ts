486a6f4f390a74d78793e1c48ba9281f95a5c286
Evgeniy <edubskiy@openwaygroup.com>
2020-06-21 03:09:53 +0300

    Added method metadata

--- src/decorators/routes.ts
+++ src/decorators/routes.ts
@@ -1,7 +1,20 @@
+import { Methods } from './methods';
 import 'reflect-metadata'
+import { router } from '../routes/routes'
 
-export function get(path: string) {
-  return (target: any, key: string, desc: PropertyDescriptor) => {
-    Reflect.defineMetadata('path', path, target, key)
-  }
+function routeBinder(method: string) {
+  return function(path: string) {
+    return (target: any, key: string, desc: PropertyDescriptor) => {
+      Reflect.defineMetadata('path', path, target, key)
+      Reflect.defineMetadata('method', 'get', target, key)
+    }
+  }  
 }
+
+
+
+export const get = routeBinder(Methods.get)
+export const put = routeBinder(Methods.put)
+export const post = routeBinder(Methods.post)
+export const del = routeBinder(Methods.del)
+export const patch = routeBinder(Methods.patch)
