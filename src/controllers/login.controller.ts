1d3be93e1a2d681c1c1182a13de9cab124d5fb70
Evgeniy <edubskiy@openwaygroup.com>
2020-06-22 19:06:14 +0200

    Decorated all routes with new style, removed old routes, code clean up

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -44,4 +44,10 @@ class LoginController {
       res.send('Invalid email or password');
     }
   }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response) {
+    req.session = null;
+    res.redirect('/');
+  }
 }
--- src/decorators/routes.ts
+++ src/decorators/routes.ts
@@ -1,21 +1,18 @@
 import { MetadataKeys } from './metadata.keys';
 import { Methods } from './methods';
-import 'reflect-metadata'
-import { router } from '../routes/routes'
+import 'reflect-metadata';
 
 function routeBinder(method: string) {
-  return function(path: string) {
+  return function (path: string) {
     return (target: any, key: string, desc: PropertyDescriptor) => {
-      Reflect.defineMetadata(MetadataKeys.path, path, target, key)
-      Reflect.defineMetadata(MetadataKeys.method, method, target, key)
-    }
-  }  
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
+    };
+  };
 }
 
-
-
-export const get = routeBinder(Methods.get)
-export const put = routeBinder(Methods.put)
-export const post = routeBinder(Methods.post)
-export const del = routeBinder(Methods.del)
-export const patch = routeBinder(Methods.patch)
+export const get = routeBinder(Methods.get);
+export const put = routeBinder(Methods.put);
+export const post = routeBinder(Methods.post);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
